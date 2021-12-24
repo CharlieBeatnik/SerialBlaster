1fcdf990b4beec971ee586e7eb803355a5e4b385
Andrew Dennison <andy@denno.co.uk>
2021-12-24 13:22:32 +0000

    It's now possible to add an optional parameter at the end of the send command to signify the number of repeats

--- SerialBlasterAtomLite/SerialBlasterAtomLite.ino
+++ SerialBlasterAtomLite/SerialBlasterAtomLite.ino
@@ -44,7 +44,7 @@ void loop()
     in = Serial.readStringUntil('\r');
 
     //Example
-    //send nec 0x3EC14DB2
+    //send nec 0x3EC14DB2 2
     char *action = strtok((char*)in.c_str(), " ");
     if (action != NULL)
     {
@@ -58,9 +58,21 @@ void loop()
             char *command = strtok(NULL, " ");
             if(command != NULL)
             {
+              int numRepeats = 0;
+              char *repeat = strtok(NULL, " ");
+              if(repeat != NULL)
+              {
+                numRepeats = atoi(repeat);
+              }
+              
               unsigned long commandUL;
               commandUL = strtoul(command, NULL, 16);
-              IrSender.sendNEC(commandUL, 32);
+              commandUL = reverse(commandUL);
+              
+              char address = (char)commandUL;
+              char command = (char)(commandUL >> 16);
+              IrSender.sendNEC(address, command, numRepeats);
+
               Serial.println("OK");
 
               if(flashOnSend)
@@ -112,4 +124,13 @@ void loop()
 
   delay(50);
   M5.update();    //Read the press state of the key.
+}
+
+unsigned long reverse(unsigned long x)
+{
+    x = (((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1));
+    x = (((x & 0xcccccccc) >> 2) | ((x & 0x33333333) << 2));
+    x = (((x & 0xf0f0f0f0) >> 4) | ((x & 0x0f0f0f0f) << 4));
+    x = (((x & 0xff00ff00) >> 8) | ((x & 0x00ff00ff) << 8));
+    return((x >> 16) | (x << 16));
 }
